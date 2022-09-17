68f513e0a31b77026d2db5869b7100c0b9b157eb
João Marcelo Simão de Castro <jm.castro@usp.br>
2022-09-17 17:49:48 -0300

    feat: add middleware to handle with errors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3333;
+const PORT: number = Number(process.env.PORT || 3333);
 app.listen(PORT, () => {
   console.log(`O servidor subiu na porta ${PORT}`);
 });
